19f18fab3d0279efe545ff2ba9725747cc4f8efd
shwetapurushe <shwetapurushe@gmail.com>
2016-08-16 18:24:21 -0400

    gulp with concatenation only

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Shweta on 8/16/16.
+ */
+/**
+ * gulp build file for dcbi app
+ * @author shweta purushe
+ */
+
+var gulp = require('gulp');
+
+//including the concat plugin
+var concat = require('gulp-concat');
+
+//defining the concat task
+gulp.task('concatScripts', function(){
+	return gulp.src('src/**/*.js')
+		.pipe(concat('dcbi.js'))
+		.pipe(gulp.dest('dist'));
+});
+
+//default task which is the wrapper for all tasks
+gulp.task('default', ['concatScripts']);
